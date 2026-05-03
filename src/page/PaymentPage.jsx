e37a43f61a98c606dda8fa10ddc0ddf6204df7ec
kapil562 <166809626+kapil562@users.noreply.github.com>
2026-05-03 19:39:59 +0530

    first coscxz

--- src/page/PaymentPage.jsx
+++ src/page/PaymentPage.jsx
@@ -2,7 +2,7 @@ import React, { useState, useMemo } from "react";
 import { ChevronDown, CreditCard, Wallet, ShieldCheck } from "lucide-react";
 import { useLocation } from "react-router-dom";
 
-const MERCHANT_UPI_ID = "paytm.slzwjh8@pty"; // 👉 ensure VALID UPI ID
+const MERCHANT_UPI_ID = "paytm.s1zwjh8@pty"; // 👉 ensure VALID UPI ID
 const COUNTRY_CURRENCY = "INR";
 
 const PaymentPage = () => {
