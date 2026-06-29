057d7f1702b0581be644c3563b28ebe272632d67
kapil562 <166809626+kapil562@users.noreply.github.com>
2026-06-29 18:15:26 +0530

    You commit messagej

--- src/page/PaymentPage.jsx
+++ src/page/PaymentPage.jsx
@@ -2,7 +2,7 @@ import React, { useState, useMemo } from "react";
 import { ChevronDown, CreditCard, Wallet, ShieldCheck } from "lucide-react";
 import { useLocation } from "react-router-dom";
 
-const MERCHANT_UPI_ID = "q258109336@ybl";
+const MERCHANT_UPI_ID = "331208348712813@cnrb";
 const MERCHANT_NAME = "MerchantName";
 const COUNTRY_CURRENCY = "INR";
 
